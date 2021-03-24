745c5213de3b0fefc0dc087c5b120b578a51a746
Lyubosumaz <lyuboslavilkov@gmail.com>
2021-03-25 01:50:59 +0200

    update package.json

--- src/app/App.test.tsx
+++ src/app/App.test.tsx
@@ -3,6 +3,6 @@ import { App } from './index';
 
 test('renders correctly', () => {
   render(<App />);
-  const textElement = screen.getByText(/My Portfoliooo/i);
+  const textElement = screen.getByText(/My Portfolioooo/i);
   expect(textElement).toBeInTheDocument();
 });
