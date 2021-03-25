3f79e040920dce43faef66b1389e061a9689b5e0
Lyubosumaz <lyuboslavilkov@gmail.com>
2021-03-25 02:51:29 +0200

    Updates

--- src/app/App.test.tsx
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import { App } from './index';
-
-test('renders correctly', () => {
-  render(<App />);
-  const textElement = screen.getByText(/My Portfoliooooooo/i);
-  expect(textElement).toBeInTheDocument();
-});
