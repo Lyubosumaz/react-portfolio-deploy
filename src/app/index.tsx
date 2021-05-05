a3d026b6252c8b5a9fae408e8f088d7b39f30a85
Lyubosumaz <lyuboslavilkov@gmail.com>
2021-05-05 03:18:50 +0300

    big overall update

---
+++ src/app/index.tsx
@@ -0,0 +1,15 @@
+import './App.css';
+
+type AppProps = {}
+
+export const App = ({ }: AppProps) => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>My Portfolio</p>
+        <p>Have problems with branches</p>
+        <p>Testing the branches</p>
+      </header>
+    </div>
+  );
+}
